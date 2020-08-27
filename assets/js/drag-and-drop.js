eac0360dae5179d4ec05d8b3c1965aacd913f87b
Attila Nagy <attila@northpoint.io>
2020-08-27 17:45:23 +0200

    init

---
+++ assets/js/drag-and-drop.js
@@ -0,0 +1,12 @@
+var DragAndDrop = DragAndDrop || {};
+
+DragAndDrop.Events = {
+    OnLoad: function(event) {
+    },
+
+    DragStart: function(event) {
+    }, 
+
+    DragEnd: function(event) {
+    }
+}
